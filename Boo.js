d79a8113401b20b33edc75c5d8ab9f7c423bb6d9
Tetiana <t.pasichnyk@ukr.net>
2020-05-12 13:44:50 +0300

    2

--- Boo.js
+++ Boo.js
@@ -1,15 +1,20 @@
 function booWho(bool) {
-    if(bool==true){
-        console.log('1')
-        return bool==true;
+    if(bool===1){
+        console.log(bool==0);
+        return bool==0
+
     }
     else if(bool==false){
         console.log('2')
         return bool==false;
     }
-    else if(bool==1){
-        return bool==0
+    else if(bool==true){
+        console.log('1')
+        return bool==true;
+    }
+    else{
+        return false
     }
 }
-
-booWho(false);
+booWho(1);
+// booWho(false);
